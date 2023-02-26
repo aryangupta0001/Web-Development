56ff18bec8cdd46d2903bf33acc22e209c6479f7
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-02-26 22:41:12 +0530

    Create os_module.js

---
+++ backend - js/node-js/os_module.js
@@ -0,0 +1,3 @@
+const os = require("os");   // os is a built-in module of node-js, it performs operations related to O.S
+
+console.log(os.freemem());    // freemem() is a function of os module, which returns available System Memory (RAM).
