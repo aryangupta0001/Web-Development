2008e7f5c498f6cabb30b2204ac5a2f5de35b522
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-02-27 00:12:51 +0530

    Update os_module.js

--- backend - js/node-js/os_module.js
+++ backend - js/node-js/os_module.js
@@ -1,3 +1,10 @@
-const os = require("os");   // os is a built-in module of node-js, it performs operations related to O.S
+const os = require("os");       // os is a built-in module of node-js, it performs operations related to O.S
 
-console.log(os.freemem());    // freemem() is a function of os module, which returns available System Memory (RAM).
+console.log(os.freemem());      // freemem() is a function of os module, which returns available System Memory (RAM).
+console.log(os.homedir());      // homedir() is a function of os module, which returns the Home Directory of the Computer.
+console.log(os.hostname());     // hostname() function returns the name of the system.
+console.log(os.platform());     // platform() function returns the name of the os  pplatform.
+console.log(os.release());      // release() function returns the release of the os.
+console.log(os.type());         // release() function returns the release of the os.
+console.log(os.totalmem());     // release() function returns the release of the os.
+console.log(os.uptime());     // release() function returns the release of the os.
