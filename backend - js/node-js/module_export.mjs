b2a4f50a725807945fab669b8a4f04b6204cd693
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-02-28 01:42:26 +0530

    Create module_export.mjs
    This file is required by mdule_import.js

---
+++ backend - js/node-js/module_export.mjs
@@ -0,0 +1,12 @@
+export function Hello(){                                                // export keyworrd is used to export ES6 modules.
+    console.log("Hello, this is exported as ES6");
+}
+
+export function hi(){
+    console.log("hi, this is exported as ES6");
+}
+
+
+export default function exp(){                                          // default keyword represents the modules that will be exported by-default
+    console.log("Hello, this is exported as default");
+}
