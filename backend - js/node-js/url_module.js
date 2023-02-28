2134ab943962a1565c7e5031f3a41cb5fc57c115
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-01 00:48:18 +0530

    Update url_module.js

--- backend - js/node-js/url_module.js
+++ backend - js/node-js/url_module.js
@@ -1,9 +1,10 @@
 import url from "url";
 
-const myURL = new URL('https://example.org');
+const myURL = new URL('https://example.org:5500');      // new URL() sets he origin of the url, we can also give the port in the origin of url.
 myURL.pathname = '/a/b/c';                              // .pathname sets the path of a url
 myURL.search = '?d=e';                                  // .search sets the search parameters
 myURL.hash = '#fgh';
+myURL.port = 80;                                        // the port can also be set with the help of .port.
 
 console.log(myURL);                                     // the contructed url will show all the component of a url
 console.log(myURL.href);                                // .href shows the complete contructed url
