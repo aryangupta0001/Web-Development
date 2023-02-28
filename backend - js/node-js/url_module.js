a1a4a1d78d4c093ded01b36221edd6898194c869
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-01 00:44:27 +0530

    Update url_module.js

--- backend - js/node-js/url_module.js
+++ backend - js/node-js/url_module.js
@@ -1,9 +1,9 @@
 import url from "url";
 
 const myURL = new URL('https://example.org');
-myURL.pathname = '/a/b/c';
-myURL.search = '?d=e';
+myURL.pathname = '/a/b/c';                              // .pathname sets the path of a url
+myURL.search = '?d=e';                                  // .search sets the search parameters
 myURL.hash = '#fgh';
 
-console.log(myURL);
-console.log(myURL.href);
+console.log(myURL);                                     // the contructed url will show all the component of a url
+console.log(myURL.href);                                // .href shows the complete contructed url
