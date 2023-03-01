0dfa48d1a79f02d21cdfffd43aa87484c8528ffa
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-01 20:30:48 +0530

    Create console_obbject.js

---
+++ javascript/console_obbject.js
@@ -0,0 +1,3 @@
+console.log(console);               // console object has various functions which are related to browser's console tab.
+
+console.log("Hello, this is console.log() function")            // console.log() function is used to log something on the console.
