7504597a32bdf3671c678507ea8e182e76fae247
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-04 21:22:14 +0530

    Create index.js

---
+++ custom module/index.js
@@ -0,0 +1,7 @@
+const custom_module = require("./module");            // to import a Coomon JS module
+
+console.log(custom_module);                           // this will print all the objects imported.
+
+
+
+console.log(custom_module.sum([2, 3, 4]));            //
