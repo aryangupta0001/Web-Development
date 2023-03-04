f6d1c250ef189e3ae260255309166dff48a7d225
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-04 21:24:30 +0530

    Update index.js

--- custom module/index.js
+++ custom module/index.js
@@ -2,6 +2,4 @@ const custom_module = require("./module");            // to import a Coomon JS m
 
 console.log(custom_module);                           // this will print all the objects imported.
 
-
-
-console.log(custom_module.sum([2, 3, 4]));            //
+console.log(custom_module([2, 3, 4]));                // if only one function is exported from the module, then it will be imported as the importing variable-name itself.
