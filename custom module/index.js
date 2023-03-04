460b16734807e246df0f7ed07b9c268a5293ee02
Aryan Gupta <65766190+aryangupta0001@users.noreply.github.com>
2023-03-04 21:30:13 +0530

    Update index.js

--- custom module/index.js
+++ custom module/index.js
@@ -2,4 +2,6 @@ const custom_module = require("./module");            // to import a Coomon JS m
 
 console.log(custom_module);                           // this will print all the objects imported.
 
-console.log(custom_module([2, 3, 4]));                // if only one function is exported from the module, then it will be imported as the importing variable-name itself.
+// console.log(custom_module([2, 3, 4]));             // if only one function is exported from the module, then it will be imported as the importing variable-name itself.
+
+console.log(custom_module.sum([2, 3, 4]));            // if multiplle things are exported from the moduule, then they are exported as a object, and the object s  imported as the importing variable-name itself. So we have to use the "dot(.)" operator to use the specific function from the object.
