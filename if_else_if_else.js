e8d52ae9a8de6aa43d26f65f45dead52b9794a24
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-06-09 17:17:30 +0530

    updated if-else ladder

--- if_else_if_else.js
+++ if_else_if_else.js
@@ -1,8 +1,22 @@
-const prompt=require("prompt-sync")({sigint:true}); 
+const prompt=require("prompt-sync")({sigint:true});             // we need to import "prompt-sync" to use prompts in our program, this is not needed while using Browser's console.
 
 let a = prompt("Enter the no. : ");
 
 a = Number.parseInt(a);
+
+
+/* Syntaxx -->
+
+if (condition)
+{
+    // block of code to be executed if the condition is true
+}
+
+else{
+    // block of code to be executed if the condition is false
+}
+
+*/
 
 if (a>10)
 {   console.log(a + " is greater than 10");
@@ -15,3 +29,6 @@ else if (a==10)
 else
 {   console.log(a + " is not greater than 10");
 }
+
+
+// if, if - else & if - else if - else is same as in C++ & Java.
